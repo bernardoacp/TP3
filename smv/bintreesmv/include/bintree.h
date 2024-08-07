34f09296c33aff1a2e5ad18f6001030a75c2afb5
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-08-07 10:40:25 -0300

    Adicionar arquivos auxiliares.

---
+++ smv/bintreesmv/include/bintree.h
@@ -0,0 +1,17 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "node.h" 
+
+void bintree_create(long numnodes);
+nodeaddr_t bintree_search(nodekey_t k, ptr_node_t pn);
+nodeaddr_t bintree_insert(nodekey_t k, ptr_node_t pn);
+void bintree_remove(nodekey_t k, ptr_node_t pn);
+void bintree_dump();
+void bintree_destroy();
+
+
+#endif
