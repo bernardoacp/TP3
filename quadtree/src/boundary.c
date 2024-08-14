b5ce57db543fe63bb749fbad21a68e8501ca0bc8
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-08-14 11:03:31 -0300

    Remover diretorio com implementacao padrao da quadtree.

--- quadtree/src/boundary.c
+++
@@ -1,7 +0,0 @@
-#include "boundary.h"
-
-bool boundary_contains(Boundary* bd, double x, double y)
-{
-    return (x >= bd->x_min && x < bd->x_max &&
-            y >= bd->y_min && y < bd->y_max);
-}
